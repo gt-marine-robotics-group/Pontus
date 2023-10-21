87cf98d00082f89946166f873104f954a938be20
Matthew <mwoodward@gatech.edu>
2023-10-21 19:10:39 -0400

    Almost working localization, IMU seems to be upside down for some reason

--- pontus_localization/setup.py
+++ pontus_localization/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'pontus_localization'
@@ -10,6 +12,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Include all launch files.
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*[pxy][yma]*'))),
+        (os.path.join('share', package_name,  'config'), glob('config/*.yaml'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
