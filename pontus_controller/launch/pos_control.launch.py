38e3b4555e5e17a80a91c83ba452f5f12a327ecd
Eric Dong <belericant@gmail.com>
2024-02-22 13:31:41 -0500

    add waypoint controller

---
+++ pontus_controller/launch/pos_control.launch.py
@@ -0,0 +1,29 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import LaunchConfiguration
+
+def generate_launch_description():
+
+    pkg_pontus_controller = get_package_share_directory('pontus_controller')
+
+    return LaunchDescription([
+        Node(
+            package='pontus_controller',
+            executable='thruster_controller',
+            output='screen'
+        ),
+        Node(
+            package='pontus_controller',
+            executable='position_controller',
+            output='screen'
+        ),
+        Node(
+            package='pontus_controller',
+            executable='velocity_controller',
+            output='screen'
+        )
+    ])
