b4ef42d079deb53f0d28a842eacb6bdc26b26399
Chris Tio <chxtio@gmail.com>
2024-03-05 01:16:10 -0800

    Add launch file for remote control

---
+++ pontus_controller/launch/rc.launch.py
@@ -0,0 +1,20 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    pkg_pontus_controller = get_package_share_directory('pontus_controller')
+    params = os.path.join(pkg_pontus_controller,'config','joystick.yaml')
+
+    joy_node = Node(
+            package='joy',
+            executable='joy_node',
+            parameters=[params],
+         )
+
+    return LaunchDescription([
+        joy_node       
+    ])
