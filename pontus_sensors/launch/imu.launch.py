7c03230b41d1a235011db3068d292f1f9b09e9d8
JeffreyFang <jeff300fang@gmail.com>
2024-03-06 04:05:39 +0200

    Imu launch (#34)
    * pontus launch files
    
    * added imu monitor
    
    * fixed issues with monitor
    
    * Fixed termination issue
    
    * comments
    
    * added power cycling, made launch file
    
    * fix comment
    
    * remove unnecessary setup.py and fix some drosdeps
    
    * move imu_monitor to pontus_sensors
    
    ---------
    
    Co-authored-by: jfang301 <jfang301@gatech.edu>
    Co-authored-by: Manuel Roglan <manueljoseph113@gmail.com>

---
+++ pontus_sensors/launch/imu.launch.py
@@ -0,0 +1,22 @@
+import os
+
+import ament_index_python
+
+from launch import LaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import IncludeLaunchDescription, ExecuteProcess
+from launch_ros.actions import Node, SetRemap
+
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        # Launch the imu_monitor_node
+        Node(
+            package='pontus_sensors',
+            executable='imu_monitor.py',
+            name='imu_monitor',
+        )
+    ])
