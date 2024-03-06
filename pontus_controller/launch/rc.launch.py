f82b81dc605eb8c856ae1080c81094d6564a9214
Manuel Roglan <manueljoseph113@gmail.com>
2024-03-05 21:29:24 -0500

    passing yaml config to node and separating between auv and sim config

--- pontus_controller/launch/rc.launch.py
+++ pontus_controller/launch/rc.launch.py
@@ -1,5 +1,7 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 
 import os
 from ament_index_python.packages import get_package_share_directory
@@ -7,18 +9,23 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
 
     pkg_pontus_controller = get_package_share_directory('pontus_controller')
-    params = os.path.join(pkg_pontus_controller,'config','joystick.yaml')
+
+    auv_arg = DeclareLaunchArgument('auv', default_value='auv')
+    auv_config = LaunchConfiguration('auv', default='auv')
+
+    params = (pkg_pontus_controller, 'config', auv_config, 'joystick.yaml')
 
     return LaunchDescription([
+        auv_arg,
         Node(
             package='teleop_twist_joy', 
             executable='teleop_node',
             name = 'teleop_node',
-            parameters=[{'ros__parameters': params}],
+            parameters=[params],
         ),
         Node(
             package='joy',
             executable='joy_node',
-            parameters=[{'ros__parameters': params}],
+            parameters=[params],
         )               
     ])
