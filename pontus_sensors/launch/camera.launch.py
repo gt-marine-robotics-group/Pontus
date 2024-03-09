c6423228c08332b15e473bb267a85f7b16d9a83e
Manuel Roglan <manueljoseph113@gmail.com>
2024-03-09 16:31:31 -0500

    launching camera republish from pontus_sensors

--- pontus_sensors/launch/camera.launch.py
+++ pontus_sensors/launch/camera.launch.py
@@ -15,19 +15,10 @@ def generate_launch_description():
     pontus_sensors_share = get_package_share_directory('pontus_sensors')
     pontus_perception_share = get_package_share_directory('pontus_perception')
     oakd_share = get_package_share_directory('depthai_ros_driver')
-    
-    imu_arg = DeclareLaunchArgument('imu', default_value='gx3')
-    imu_config = LaunchConfiguration('imu', default='gx3')
-
-    imu_config_str = 'gx3'
-    for arg in sys.argv:
-        if arg.startswith('imu:='):
-            imu_config_str = arg.split(':=')[1]
 
     ld = list()
-    ld.append(imu_arg)
 
-    with open(f'{pontus_perception_share}/config/camera_config.yaml', 'r') \
+    with open(f'{pontus_perception_share}/config/auv/camera_config.yaml', 'r') \
         as stream:
         camera_data = yaml.safe_load(stream)
 
@@ -40,15 +31,6 @@ def generate_launch_description():
                     PythonLaunchDescriptionSource(os.path.join(oakd_share, 'launch/camera.launch.py')),
                 )
             )
-            # uncomment to use oakd imu
-            # if imu_config_str == 'oak': # using oak-d imu
-            #     ld.append(Node(
-            #         package='virtuoso_sensors',
-            #         executable='gx3_republish',
-            #         remappings=[
-            #             ('/imu/data', '/oak/imu/data')
-            #         ]
-            #     ))
     
     for i, frame in enumerate(camera_data['camera_config']['all_camera_frames']):
         ld.append(
@@ -66,7 +48,7 @@ def generate_launch_description():
     for i, topic in enumerate(camera_data['camera_config']['all_camera_base_topics']):
         ld.append(
             Node(
-                package='virtuoso_sensors',
+                package='pontus_sensors',
                 executable='camera_republish.py',
                 name=f'{topic[topic.rfind("/") + 1:]}_republish',
                 remappings=[
