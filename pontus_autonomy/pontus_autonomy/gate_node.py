b8a7e692ee4620c0757a00ace545018cc5d4862a
Matthew Woodward <mwoodward@gatech.edu>
2024-03-31 16:09:52 -0400

    Working gate task in sim

--- pontus_autonomy/pontus_autonomy/gate_node.py
+++ pontus_autonomy/pontus_autonomy/gate_node.py
@@ -1,5 +1,6 @@
 import rclpy
 from rclpy.node import Node
+from rclpy.duration import Duration
 import numpy as np
 
 import tf2_ros
@@ -18,16 +19,7 @@ class GateNode(Node):
     def __init__(self):
         super().__init__('gate_controller')
 
-        # ROS infrastructure
         self.cmd_vel_pub = self.create_publisher(Twist, '/cmd_vel', 1)
-
-        ''' # Don't need this right now
-        self.odom_sub = self.create_subscription(
-          Odometry,
-          '/pontus/odometry',
-          self.odometry_callback,
-          10)
-        '''
 
         qos_profile = QoSProfile(
                           reliability=QoSReliabilityPolicy.RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT,
@@ -45,63 +37,98 @@ class GateNode(Node):
 
         self.task_completed = False
 
-        self.gate_color = cv2.cvtColor(np.uint8([[[100, 0, 0]]]), cv2.COLOR_RGB2HSV)
-        self.upper_tolerance = cv2.cvtColor(np.uint8([[[130, 20, 20]]]), cv2.COLOR_RGB2HSV)
-        self.lower_tolerance = cv2.cvtColor(np.uint8([[[70, 0, 0]]]), cv2.COLOR_RGB2HSV)
+        # We are driving towards the gate
+        self.approaching_gate = False
 
+        # We are passing through the gate, keep driving fowards
+        self.drive_time = None
+
+        # values can be near 0 or 180 so we have to threshold twice then merge
+        self.gate_hsv1 = np.uint8([0, 255, 100])
+        self.gate_hsv2 = np.uint8([180, 255, 100])
+
+        # around 0
+        self.lower1 = self.gate_hsv1 - np.uint8([0, 20, 20,])
+        self.upper1 = self.gate_hsv1 + np.uint8([20, 0, 20,])
+
+        # around 180
+        self.lower2 = self.gate_hsv2 - np.uint8([20, 20, 20,])
+        self.upper2 = self.gate_hsv2 + np.uint8([0, 0, 20,])
+
+        # positions of the red bars on the gate
+        self.markers = []
 
     def camera_callback(self, data):
      
         # Convert ROS Image message to OpenCV image
         hsv_frame = cv2.cvtColor(self.bridge.imgmsg_to_cv2(data), cv2.COLOR_RGB2HSV)
+        height, width, _ = hsv_frame.shape
 
-        # This is finding black not red :'(
-        mask = cv2.inRange(hsv_frame, self.lower_tolerance, self.upper_tolerance)
+        mask1 = cv2.inRange(hsv_frame, self.lower1, self.upper1)
+        mask2 = cv2.inRange(hsv_frame, self.lower2, self.upper2)
+        mask = mask1 + mask2
 
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         by_size = sorted(contours, key = lambda c: cv2.contourArea(c))
 
-        canvas = cv2.cvtColor(hsv_frame.copy(), cv2.COLOR_HSV2BGR)
+        self.markers = []
+        for contour in by_size:
+            # only consider the 2 largest blobs
+            # This can be tweaked if we want to consider the two side bars and the middle marker
+            if len(self.markers) > 1:
+                break
 
-        print("blobs: ")
-        for i in by_size:
-            M = cv2.moments(i)
-            # Mostly working, devided by 0 for some reason
-            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-            cv2.circle(canvas, center, 2, (0,0,255), -1)
+            M = cv2.moments(countour)
 
-            print(center)
+            # Only use contours with nonzero area
+            if M["m00"] != 0:
+                center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+                #cv2.circle(canvas, center, 2, (0,0,255), -1)
+                self.markers.append(center)
 
-        print("-----------------------")
 
-        cv2.imshow("test", canvas)
-        cv2.waitKey(0)
-        
-    '''
-    def odometry_callback(self, msg):
-
-        # Get the current velocities from odometry
-        v_linear = np.array([msg.twist.twist.linear.x, msg.twist.twist.linear.y, msg.twist.twist.linear.z])
-        v_angular = np.array([msg.twist.twist.angular.x, msg.twist.twist.angular.y, msg.twist.twist.angular.z])
-
-        # Compute the error between desired velocity and current velocity
-        linear_err = self.cmd_linear - v_linear
-        angular_err = self.cmd_angular - v_angular
-
-        # Compute and publish the body accelerations
         msg = Twist()
-        msg.linear.x = self.pid_linear[0](linear_err[0], self.get_clock().now() - self.prev_time)
-        msg.linear.y = self.pid_linear[1](linear_err[1], self.get_clock().now() - self.prev_time)
-        msg.linear.z = self.pid_linear[2](linear_err[2], self.get_clock().now() - self.prev_time)
 
-        msg.angular.x = self.pid_angular[0](angular_err[0], self.get_clock().now() - self.prev_time)
-        msg.angular.y = self.pid_angular[1](angular_err[1], self.get_clock().now() - self.prev_time)
-        msg.angular.z = self.pid_angular[2](angular_err[2], self.get_clock().now() - self.prev_time)
+        # Drive towards the center of the gate
+        if len(self.markers) == 2:
+            print("Drive Mode")
+            self.approaching_gate = True
+            self.drive_time = self.get_clock().now()
 
-        self.cmd_accel_pub.publish(msg)
+            target_x = (self.markers[0][0] + self.markers[1][0]) / 2
+            target_y = (self.markers[0][1] + self.markers[1][1]) / 2
 
-        self.prev_time = self.get_clock().now()
-    '''
+            msg.linear.x = 1.0
+
+            # Pass a bit lower so we don't run into the signs
+            msg.linear.z = 0.5 if target_y < height/4 else -0.5
+
+            # Rotate towards the center
+            msg.angular.z = 1.0 if target_x < width/2 else -1.0
+
+        else:
+            # Just keep driving forward for a bit to pass through the gate
+            if self.approaching_gate:
+
+                if self.get_clock().now() - self.drive_time > Duration(seconds=0.5):
+                    print("Task Complete")
+                    self.task_completed = True
+                    msg.linear.x = 0.0
+                else:
+                    print("Passing Through Gate")
+                    msg.linear.x = 0.5
+
+            # Look for the gate
+            else:
+                print("Looking for Gate")
+
+                # Spin towards gate
+                if len(self.markers) == 1:
+                   msg.angular.z = 2.0 if self.markers[0][0] < width/2 else -2.0
+                else:
+                    msg.angular.z = 2.0
+
+        self.cmd_vel_pub.publish(msg)
 
 def main(args=None):
     rclpy.init(args=args)
