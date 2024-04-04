2595dccb57e7024b4d248b4da8478cccfd65d0cb
Jeff300fang <jeff300fang@gmail.com>
2024-04-04 14:24:19 -0400

    Some particle filter

---
+++ pontus_localization/pontus_localization/pf_constants.py
@@ -0,0 +1,19 @@
+# This is the maximum distance new particles should be from the assumed position of thesub
+# This value should be no greater than half the size of a grid cell
+CUTOFF_DISTANCE = 5.0
+
+# These values are used to sample from a Gaussian distribution to simulate noise in odometry
+TRANS_MU = 0
+TRANS_SIGMA = 0.5
+
+ROTATE_MU = 0
+ROTATE_SIGMA = 0.2
+ROTATE_SAMPLE_MAX = 1
+
+# The number of particles to use in the particle filter
+NUM_PARTICLES = 50
+
+# This is used to demonstrate the uncertainty in our marker comparison measures
+# This is used for the marker likelihood function 
+MARKER_DISTANCE_SIGMA = 0.3
+MARKER_ANGLE_SIGMA = 0.5
