8203398766afa5ee1a7514e78abda3bc0c5d46c2
Jeff300fang <jeff300fang@gmail.com>
2024-04-12 00:44:16 -0400

    command

--- pontus_localization/pontus_localization/particle_filter.py
+++ pontus_localization/pontus_localization/particle_filter.py
@@ -16,6 +16,8 @@ from sensor_msgs.msg import PointCloud2, PointField
 import numpy as np
 from .pf_constants import *
 
+# ros2 run tf2_ros static_transform_publisher --x 0 --y 0 --z 0 --qx 0 --qy 0 --qz 0 --qw 1 --frame-id map --child-frame-id world
+
 class Particle:
     def __init__(self, x = 0, y = 0, z = 0, yaw = 0):
         self.position = np.zeros((3,))
