fdd9f43086278a27360db1e84ff0052f7908bd11
Jeff300fang <jeff300fang@gmail.com>
2025-02-20 23:37:25 -0500

    Add stereo and yolo launch to detection services launch

--- pontus_perception/launch/detection_services.launch.py
+++ pontus_perception/launch/detection_services.launch.py
@@ -4,6 +4,8 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import IncludeLaunchDescription
 import sys
 
 def generate_launch_description():
@@ -16,8 +18,20 @@ def generate_launch_description():
             auv_config_str = arg.split(':=')[1]
 
     gate_detection_config_file = f'{pontus_perception_share}/config/{auv_config_str}/gate_detection.yaml'
+    
+    _STEREO_LAUNCH_FILE = os.path.join(get_package_share_directory('pontus_perception'), 'launch', 'stereo.launch.py')
+    _YOLO_LAUNCH_FILE = os.path.join(get_package_share_directory('pontus_perception'), 'launch', 'yolo.launch.py')
 
     return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(_STEREO_LAUNCH_FILE),
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(_YOLO_LAUNCH_FILE),
+            launch_arguments={
+                'auv': auv_config_str,
+            }.items()
+          ),
         Node(
             package='pontus_perception',
             executable='gate_detection',
@@ -25,4 +39,5 @@ def generate_launch_description():
             output='screen',
             parameters=[gate_detection_config_file],
         ),
+        
     ])
