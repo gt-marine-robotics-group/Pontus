4ec1da5181986fb9da3e2a270505f1988b69499a
MitchellTurton <mbturton33@gmail.com>
2025-08-15 14:31:14 -0700

    Run info practice 1

--- pontus_autonomy/pontus_autonomy/helpers/run_info.py
+++ pontus_autonomy/pontus_autonomy/helpers/run_info.py
@@ -8,22 +8,22 @@ from pontus_autonomy.helpers.run_info_helpers import *
 # ------- Semantic Map Info -------
 
 # ------ Gate Task ------
-GATE_CENTER_X = 7.5
-GATE_CENTER_Y = 3.0
+GATE_CENTER_X = -0.75
+GATE_CENTER_Y = -4.57
 
 DEFAULT_GATE_DEPTH = POOL_DEPTH + 0.6 + 0.3
 
 gate_left = SemanticMapObject(
     label=SemanticObject.LeftGate,
-    x=GATE_CENTER_X - 1.5,
-    y=GATE_CENTER_Y + 0.0,
+    x=GATE_CENTER_X + 1.5,
+    y=GATE_CENTER_Y - 0.0,
     depth=DEFAULT_GATE_DEPTH
 )
 
 gate_right = SemanticMapObject(
     label=SemanticObject.RightGate,
-    x=GATE_CENTER_X + 1.5,
-    y=GATE_CENTER_Y + 0.0,
+    x=GATE_CENTER_X - 1.5,
+    y=GATE_CENTER_Y - 0.0,
     depth=DEFAULT_GATE_DEPTH
 )
 
@@ -32,8 +32,8 @@ gate_right = SemanticMapObject(
 DEFAULT_SLALOM_DEPTH = POOL_DEPTH + 0.45 + 0.3
 
 # --- Slalom Set 1 ---
-RED_SLALOM_1_X = GATE_CENTER_X - 3.0
-RED_SLALOM_1_Y = GATE_CENTER_Y + 3.0
+RED_SLALOM_1_X = GATE_CENTER_X - 2.0
+RED_SLALOM_1_Y = GATE_CENTER_Y - 3.5
 
 slalom_set_1 = SlalomSet(
     red_slalom=SemanticMapObject(
@@ -45,23 +45,23 @@ slalom_set_1 = SlalomSet(
 
     white_slalom_left=SemanticMapObject(
         label=SemanticObject.SlalomWhite,
-        x=RED_SLALOM_1_X - 1.5,
-        y=RED_SLALOM_1_Y + 0.0,
+        x=RED_SLALOM_1_X + 1.2,
+        y=RED_SLALOM_1_Y - 0.3,
         depth=DEFAULT_SLALOM_DEPTH
     ),
 
     white_slalom_right=SemanticMapObject(
         label=SemanticObject.SlalomWhite,
-        x=RED_SLALOM_1_X + 1.5,
-        y=RED_SLALOM_1_Y + 0.0,
+        x=RED_SLALOM_1_X - 1.2,
+        y=RED_SLALOM_1_Y + 0.3,
         depth=DEFAULT_SLALOM_DEPTH
     )
 )
 
 
 # --- Slalom Set 2 ---
-RED_SLALOM_2_X = RED_SLALOM_1_X + 1.0
-RED_SLALOM_2_Y = RED_SLALOM_1_Y + 2.0
+RED_SLALOM_2_X = RED_SLALOM_1_X - 0.3
+RED_SLALOM_2_Y = RED_SLALOM_1_Y - 1.2
 
 slalom_set_2 = SlalomSet(
     red_slalom=SemanticMapObject(
@@ -73,23 +73,23 @@ slalom_set_2 = SlalomSet(
 
     white_slalom_left=SemanticMapObject(
         label=SemanticObject.SlalomWhite,
-        x=RED_SLALOM_2_X - 1.5,
-        y=RED_SLALOM_2_Y + 0.0,
+        x=RED_SLALOM_2_X + 1.3,
+        y=RED_SLALOM_2_Y - 0.2,
         depth=DEFAULT_SLALOM_DEPTH
     ),
 
     white_slalom_right=SemanticMapObject(
         label=SemanticObject.SlalomWhite,
-        x=RED_SLALOM_2_X + 1.5,
-        y=RED_SLALOM_2_Y + 0.0,
+        x=RED_SLALOM_2_X - 1.3,
+        y=RED_SLALOM_2_Y + 0.2,
         depth=DEFAULT_SLALOM_DEPTH
     )
 )
 
 
 # --- Slalom Set 3 ---
-RED_SLALOM_3_X = RED_SLALOM_2_X - 2.2
-RED_SLALOM_3_Y = RED_SLALOM_2_Y + 2.0
+RED_SLALOM_3_X = RED_SLALOM_2_X - 0.3
+RED_SLALOM_3_Y = RED_SLALOM_2_Y - 1.0
 
 slalom_set_3 = SlalomSet(
     red_slalom=SemanticMapObject(
@@ -101,15 +101,15 @@ slalom_set_3 = SlalomSet(
 
     white_slalom_left=SemanticMapObject(
         label=SemanticObject.SlalomWhite,
-        x=RED_SLALOM_3_X - 1.5,
-        y=RED_SLALOM_3_Y + 0.0,
+        x=RED_SLALOM_3_X + 1.2,
+        y=RED_SLALOM_3_Y - 0.3,
         depth=DEFAULT_SLALOM_DEPTH
     ),
 
     white_slalom_right=SemanticMapObject(
         label=SemanticObject.SlalomWhite,
-        x=RED_SLALOM_3_X + 1.5,
-        y=RED_SLALOM_3_Y + 0.0,
+        x=RED_SLALOM_3_X - 1.2,
+        y=RED_SLALOM_3_Y + 0.3,
         depth=DEFAULT_SLALOM_DEPTH
     )
 )
@@ -202,36 +202,54 @@ waypoints_list: list[PoseObj] = [
 
 """
 
-# waypoints_list: list[PoseObj] = [
+waypoints_list: list[PoseObj] = [
 
-#     # Start Point
-#     make_waypoint(0.0, 0.0),
 
-#     # make_waypoint(GATE_CENTER_X, GATE_CENTER_Y - 1.0),
-#     make_waypoint((gate_right.x + GATE_CENTER_X) / 2.0,
-#                   GATE_CENTER_Y - 1.25),
+    make_waypoint(GATE_CENTER_X, GATE_CENTER_Y + 2.5, depth=0.0),
 
-#     # Through the gate (slightly right of gate center)
-#     make_waypoint((gate_right.x + GATE_CENTER_X) / 2.0,
-#                   GATE_CENTER_Y + 0.75),
+    make_waypoint(GATE_CENTER_X, GATE_CENTER_Y + 2.5),
 
-#     # Slalom Task
-#     make_waypoint(slalom_enter_point.x + 0.0, slalom_enter_point.y + 0.0),
+    make_waypoint(GATE_CENTER_X, GATE_CENTER_Y + 2.5, yaw_degrees=00),
+    make_waypoint(GATE_CENTER_X, GATE_CENTER_Y + 2.5, yaw_degrees=90.0),
+    make_waypoint(GATE_CENTER_X, GATE_CENTER_Y + 2.5, yaw_degrees=180.0),
+    make_waypoint(GATE_CENTER_X, GATE_CENTER_Y + 2.5, yaw_degrees=270.0),
+    make_waypoint(GATE_CENTER_X, GATE_CENTER_Y + 2.5, yaw_degrees=360.0),
 
-#     make_waypoint(slalom_midpoint_1.x, slalom_midpoint_1.y),
+    make_waypoint((gate_right.x + GATE_CENTER_X) / 2.0,
+                  GATE_CENTER_Y + 1.25),
 
-#     make_waypoint(slalom_travel_1.x + 0.0, slalom_travel_1.y + 0.0),
+    # Through the gate (slightly right of gate center)
+    make_waypoint((gate_right.x + GATE_CENTER_X) / 2.0,
+                  GATE_CENTER_Y - 0.75),
 
-#     make_waypoint(slalom_midpoint_2.x, slalom_midpoint_2.y),
+    # Slalom Task
+    make_waypoint(slalom_enter_point.x + 0.0, slalom_enter_point.y + 0.0),
 
-#     make_waypoint(slalom_travel_2.x + 0.0, slalom_travel_2.y + 0.0),
+    make_waypoint(slalom_midpoint_1.x, slalom_midpoint_1.y),
 
-#     make_waypoint(slalom_midpoint_3.x, slalom_midpoint_3.y),
+    make_waypoint(slalom_travel_1.x + 0.0, slalom_travel_1.y + 0.0),
 
-#     # Exit Slalom
-#     # --------- VARIABLE --------
-#     make_waypoint(slalom_midpoint_3.x,
-#                   slalom_midpoint_3.y + ROBOT_LENGTH + 0.2),
+    make_waypoint(slalom_midpoint_2.x, slalom_midpoint_2.y),
+
+    make_waypoint(slalom_travel_2.x + 0.0, slalom_travel_2.y + 0.0),
+
+    make_waypoint(slalom_midpoint_3.x, slalom_midpoint_3.y),
+
+    # Exit Slalom
+    # --------- VARIABLE --------
+    # make_waypoint(slalom_midpoint_3.x,
+    #               slalom_midpoint_3.y - ROBOT_LENGTH - 0.2),
+
+    # # --------- VARIABLE --------
+    # make_waypoint(GATE_CENTER_X - 3.0, GATE_CENTER_Y + 4.0),
+
+
+    # make_waypoint((gate_right.x + GATE_CENTER_X) / 2.0,
+    #               GATE_CENTER_Y + 0.75),
+
+    # make_waypoint((gate_right.x + GATE_CENTER_X) / 2.0,
+    #               GATE_CENTER_Y - 1.25),
+]
 
 #     # Octagon
 #     make_waypoint(octagon.x, octagon.y),
@@ -252,10 +270,10 @@ waypoints_list: list[PoseObj] = [
 # ]
 
 
-waypoints_list: list[PoseObj] = [
-    make_waypoint(0, 0, yaw_degrees=0),
-    make_waypoint(0, 0, yaw_degrees=90),
-    make_waypoint(0, 0, yaw_degrees=180),
-    make_waypoint(0, 0, yaw_degrees=270),
-    make_waypoint(0, 0, yaw_degrees=360),
-]
+# waypoints_list: list[PoseObj] = [
+#     make_waypoint(0, 0, yaw_degrees=0),
+#     make_waypoint(0, 0, yaw_degrees=90),
+#     make_waypoint(0, 0, yaw_degrees=180),
+#     make_waypoint(0, 0, yaw_degrees=270),
+#     make_waypoint(0, 0, yaw_degrees=360),
+# ]
