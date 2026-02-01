d3a2eb67ee9f4ddbd1bd176b7915efd4081ebe67
Sean Fish <43331368+seantfish@users.noreply.github.com>
2026-02-01 16:11:25 -0500

    Delete pontus_bringup directory

--- pontus_bringup/scripts/record_all_topics.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# ros2 bag record -a --exclude-topic-types sensor_msgs/msg/Image "$@"
-ros2 bag record -a -x 'image_raw$' "$@"
--- pontus_bringup/scripts/teensy_reboot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo uhubctl -l 1-3.1 -p 2 -a cycle
