aaf1b824c2bb1423a966ccf840030ffaa32ff691
MitchellTurton <mbturton33@gmail.com>
2026-02-27 19:41:30 -0500

    Adjusted placement of objects in comp.world and made it the default world file in odom_simulation

---
+++ pontus_autonomy/pontus_autonomy/new_prequal.py
@@ -0,0 +1,26 @@
+
+import rclpy
+
+from typing import Optional, List
+
+from pontus_autonomy.base_run import BaseRun
+from geometry_msgs.msg import PoseStamped, Point, Quaternion
+
+# Tasks
+from pontus_autonomy.tasks.localization.submerge import Submerge
+from pontus_autonomy.tasks.prequal_gate_task import PrequalGateTask
+from pontus_autonomy.tasks.prequal_vertical_marker_task import PrequalVerticalMarkerTask
+
+from pontus_autonomy.tasks.prequal_search_gate_task import PrequalSearchTask
+
+class PrequalificationRun(BaseRun):
+    def __init__(self):
+        super().__init__("new_prequalification_run_sim")
+        
+        self.get_logger().info("Starting Prequalification Run")
+
+        # Submerge Task
+        result = self.run_task(Submerge)
+        self.get_logger().info(f"Submerge: {result}")
+
+        # Gate Prequal Task
